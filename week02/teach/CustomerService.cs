303f048ebef0c6b1de33f330238063db2f9a8fc9
natelandrum <nate178dawg@gmail.com>
2024-06-20 12:06:28 -0600

    Week 02 Team Activity

--- week02/teach/CustomerService.cs
+++ week02/teach/CustomerService.cs
@@ -11,24 +11,68 @@ public class CustomerService {
         // Test Cases
 
         // Test 1
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: Specifify the max size.
+        // Expected Result: [size=0 max_size=5 => ]
         Console.WriteLine("Test 1");
+        var cs = new CustomerService(5);
+        Console.WriteLine(cs);
 
-        // Defect(s) Found: 
+        // Defect(s) Found: None.
 
         Console.WriteLine("=================");
 
         // Test 2
-        // Scenario: 
-        // Expected Result: 
+        // Scenario: Specify an invalid max size.
+        // Expected Result: [size=0 max_size=10 => ]
         Console.WriteLine("Test 2");
+        cs = new CustomerService(0);
+        Console.WriteLine(cs);
 
-        // Defect(s) Found: 
+        // Defect(s) Found: None.
 
         Console.WriteLine("=================");
 
-        // Add more Test Cases As Needed Below
+        // Test 3
+        // Scenario: Add two customers and serve them in the correct order.
+        // Expected Result: John Doe (12345)  : My comupter is broken
+        //                  Jane Doe (54321)  : My phone is broken
+        Console.WriteLine("Test 3");
+        cs = new CustomerService(2);
+        cs.AddNewCustomer();
+        Console.WriteLine();
+        cs.AddNewCustomer();
+        Console.WriteLine();
+        cs.ServeCustomer();
+        Console.WriteLine();
+        cs.ServeCustomer();
+        Console.WriteLine();
+
+        // Defect(s) Found: Person was being removed from the queue before serving them.
+
+        Console.WriteLine("=================");
+
+        // Test 4
+        // Scenario: Try to add a person to an already full queue.
+        // Expected Result: Maximum Number of Customers in Queue.
+        Console.WriteLine("Test 4");
+        cs = new CustomerService(1);
+        cs.AddNewCustomer();
+        Console.WriteLine();
+        cs.AddNewCustomer();
+
+        // Defect(s) Found: No error raised by adding person to full queue.
+
+        Console.WriteLine("=================");
+
+        // Test 5
+        // Scenario: Try to serve a customer on an empty queue.
+        // Expected Result: Error raised: Empty queue.
+        Console.WriteLine("Test 5");
+        cs = new CustomerService(1);
+        cs.ServeCustomer();
+
+        // Defect(s) Found: No error raised when queue is empty.
+
     }
 
     private readonly List<Customer> _queue = new();
@@ -67,7 +111,7 @@ public class CustomerService {
     /// </summary>
     private void AddNewCustomer() {
         // Verify there is room in the service queue
-        if (_queue.Count > _maxSize) {
+        if (_queue.Count >= _maxSize) {
             Console.WriteLine("Maximum Number of Customers in Queue.");
             return;
         }
@@ -87,10 +131,15 @@ public class CustomerService {
     /// <summary>
     /// Dequeue the next customer and display the information.
     /// </summary>
-    private void ServeCustomer() {
-        _queue.RemoveAt(0);
-        var customer = _queue[0];
-        Console.WriteLine(customer);
+    private void ServeCustomer() {     
+        if (_queue.Count > 0) {
+            var customer = _queue[0];
+            _queue.RemoveAt(0);
+            Console.WriteLine(customer);
+        }
+
+        Console.WriteLine("Queue is empty.");
+            
     }
 
     /// <summary>
